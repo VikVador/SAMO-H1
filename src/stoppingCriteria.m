9939adb90b45094ffb0681009d93ca5724075f89
Arnaud Rémi <90702212+arnaudremi11@users.noreply.github.com>
2022-10-22 11:35:30 +0200

    6 different line search methods, + modifications

--- src/stoppingCriteria.m
+++ src/stoppingCriteria.m
@@ -1,4 +1,4 @@
-function stop = stoppingCriteria(i, x_k, x_k_1, functionID, eps, nu)
+function stop = stoppingCriteria(i, s, eps, f, nu, x_k, x_kplus1)
     %--------------
     % Documentation
     %--------------
@@ -7,37 +7,28 @@ function stop = stoppingCriteria(i, x_k, x_k_1, functionID, eps, nu)
     %
     % 1 - max_value(grad_f(x_k+1)) < eps
     % 
-    % 2 - sum(grad_f^2) < eps
+    % 2 - norm(grad_f)^2 < eps
     %
     % 3 - |f(x_k+1) - f(x_k)| < nu
-    %
+    
     % Initialization of the SC value
     stop = false;
 
     switch i
         case 1
-            % Step 1 - Computing grad
-            grad = getObjFGradVal(x_k_1, functionID);
-
             % Step 2 - Check if max value is < eps
-            if max(grad) < eps
+            if max(s) < eps
                 stop = true;
             end
 
         case 2
-            % Step 1 - Computing grad
-            grad = getObjFGradVal(x_k_1, functionID);
-
             % Step 2 - Check if sum of all grad element to ^2 is < eps
-            if sum(grad.^2) < eps
+            if norm(s)^2 < eps
                 stop = true;
             end
         
         case 3
-            % Step 1 - Computing residual
-            res = abs(getObjFVal(x_k_1, functionID) - getObjFVal(x_k, functionID));
-            disp(res);
-
+            res = abs( f(x_kplus1(1), x_kplus1(2)) - f(x_k(1), x_k(2)) );
             % Step 2 - Check if residual is small enough
             if res < nu
                 stop = true;
